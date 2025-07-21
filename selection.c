b19e4f0f1a8035a929a7f8e228840a752a0dcbca
Ernício Jermias Chilundo <ernicio.j.chilundo@gmail.com>
2025-07-21 03:15:47 +0200

    feat: add function to print contents of integer array with formatting
    - Implements  to display array elements with custom [valuekg] format
    - Useful for visualizing current state of data, such as box weights

--- selection.c
+++ selection.c
@@ -3,9 +3,20 @@
 #include <stdlib.h>
 #include <time.h>
 
+// Swap two values
 void swap(int *a, int *b)
 {
     int temp = *a;
     *a = *b;
     *b = temp;
+}
+
+// Print the current state of the boxes
+void print_array(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("[%dkg] ", arr[i]);
+    }
+    printf("\n");
 }
